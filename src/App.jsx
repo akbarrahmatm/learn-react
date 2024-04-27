1c26c61b1c2a4db860bf12d3bcbeefc765bc9ab2
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-04-27 20:16:15 +0700

    update state object using onChange event from input

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,112 @@ import Content from "./components/Content";
 import Header from "./components/Header";
 
 export default function App() {
-  const [carBrand, setCarBrand] = useState();
+  const defaultCar = {
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red",
+  };
+
+  const [car, setCar] = useState(defaultCar);
+
+  function handleChangeBrand(event) {
+    return setCar((previousState) => {
+      return { ...previousState, brand: event.target.value };
+    });
+  }
+
+  function handleChangeModel(event) {
+    return setCar((previousState) => {
+      return { ...previousState, model: event.target.value };
+    });
+  }
+
+  function handleChangeYear(event) {
+    return setCar((previousState) => {
+      return { ...previousState, year: event.target.value };
+    });
+  }
+
+  function handleChangeColor(event) {
+    return setCar((previousState) => {
+      return { ...previousState, color: event.target.value };
+    });
+  }
 
   return (
     <>
       <div className="bg-slate-700 flex flex-col h-screen">
         <Header />
         <Content>
-          <ul className="text-center my-5">
-            <li className="inline-block px-2 text-white text-lg font-medium underline">
-              <button onClick={() => setCarBrand("Ford")}>Ford</button>
-            </li>
-            <li className="inline-block px-2 text-white text-lg font-medium">
-              <button onClick={() => setCarBrand("Toyota")}>Toyota</button>
-            </li>
-            <li className="inline-block px-2 text-white text-lg font-medium">
-              <button onClick={() => setCarBrand("Hyundai")}>Hyundai</button>
-            </li>
-          </ul>
-          <h1 className="text-center text-md text-white">
-            {!carBrand ? "You haven't selected anything !" : carBrand}
-          </h1>
+          <div className="text-white p-10">
+            <h1 className="font-bold">Car From Object State</h1>
+            <ul>
+              <li>Brand : {car.brand}</li>
+              <li>Model : {car.model}</li>
+              <li>Year : {car.year}</li>
+              <li>Color : {car.color}</li>
+            </ul>
+          </div>
+
+          <div className="px-10">
+            <div className="mb-2">
+              <label className="text-white" htmlFor="brand">
+                Brand :{" "}
+              </label>
+              <input
+                className="px-2 py-1"
+                type="text"
+                name="brand"
+                id="brand"
+                placeholder="Brand"
+                onChange={handleChangeBrand}
+                value={car.brand || ""}
+              />
+            </div>
+            <div className="mb-2">
+              <label className="text-white" htmlFor="model">
+                Model :{" "}
+              </label>
+              <input
+                className="px-2 py-1"
+                type="text"
+                name="model"
+                id="model"
+                placeholder="Model"
+                onChange={handleChangeModel}
+                value={car.model || ""}
+              />
+            </div>
+            <div className="mb-2">
+              <label className="text-white" htmlFor="year">
+                Year :{" "}
+              </label>
+              <input
+                className="px-2 py-1"
+                type="text"
+                name="year"
+                id="year"
+                placeholder="Year"
+                onChange={handleChangeYear}
+                value={car.year || ""}
+              />
+            </div>
+            <div className="mb-2">
+              <label className="text-white" htmlFor="color">
+                Color :{" "}
+              </label>
+              <input
+                className="px-2 py-1"
+                type="text"
+                name="color"
+                id="color"
+                placeholder="Color"
+                onChange={handleChangeColor}
+                value={car.color || ""}
+              />
+            </div>
+          </div>
         </Content>
       </div>
     </>
